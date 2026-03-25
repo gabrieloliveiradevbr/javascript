05d73bfe3987b07dd884786b266a605f2c44eb7a
gabrieloliveira <go742743@gmail.com>
2026-03-24 21:57:55 -0300

    mais conhecimento em js

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 18
+if (idade < 16) {
+    console.log('Não pode votar')
+} else if (idade >= 16 && idade < 18) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
