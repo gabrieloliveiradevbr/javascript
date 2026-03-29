e31804a9cb777da70f750eaf41c66d768f594c88
gabrieloliveira <go742743@gmail.com>
2026-03-28 22:47:18 -0300

    javascript

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá, mundo!')
+function gerar(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('tab')
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um número')
+    } else {
+        tab.innerHTML = ''
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
